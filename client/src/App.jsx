200d823fdd52fe8fd723067adf9dd407b89ca47a
Lee Siang Yin <siangyin916@gmail.com>
2023-09-24 20:29:21 +0800

    restructure dir & boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Life Track app</h1>
+			<Button type="primary">Button</Button>
+		</div>
+	);
+};
+export default App;
--- src/App.jsx
+++
@@ -1,4 +0,0 @@
-const App = () => {
-	return <h1>Life Track app</h1>;
-};
-export default App;
